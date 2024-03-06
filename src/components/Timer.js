5537b013c6145a9212ea0837b8b32f3e478c63a0
Sidsha242 <sdemail2244@gmail.com>
2024-03-06 22:43:30 +0530

    7-3-24

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,7 +4,7 @@ import Image from "next/image"
 const Timer = () => {
   return (
     <div className="text-white w-full bg-[url('../assets/timer-back.svg')] relative overflow-x-hidden pt-10">
-            <div className="text-3xl font-custom1 z-2 text-center">TIME'S RUNNING OUT</div>
+            <div className="text-3xl font-custom1 z-2 text-center">TIMES RUNNING OUT</div>
     <Image src={right_circle} className="rotate -mr-40 z-1"></Image>
     </div>
   )

0ac54437b8b93ba8d883ea93ed44af8f4af58fc3
clumsydog09 <adityav090304@gmail.com>
2024-03-31 12:27:29 +0530

    Basic style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,19 @@ import linkedin from "../assets/linkedin.svg"
 import Image from "next/image";
 const Footer = () => {
   return (
-    <div className='bg-[#010613] pb-20 lg:flex lg:flex-col lg:justify-center'>
+    <div className='w-full h-screen bg-[#010613] pb-20 lg:flex lg:flex-col lg:justify-center'>
       <div className='flex justify-center font-custom2 text-2xl text-white lg:text-3xl'>Sponsors</div>
-        <a href="https://iecsemanipal.com/" className='flex justify-center'>
-        <Image src={footer_back}></Image>
+      <a href="https://iecsemanipal.com/" className='flex justify-center'>
+        <Image src={footer_back} alt='footer-back'></Image>
+      </a>
+      <div className='flex flex-row space-x-20 justify-center items-center'>
+        <a href="https://www.instagram.com/iecsemanipal/">
+          <Image src={insta} className='h-8 w-8' alt='insta'></Image>
         </a>
-        <div className='flex flex-row space-x-20 justify-center items-center'>
-            <a href="https://www.instagram.com/iecsemanipal/">
-                <Image src={insta} className='h-8 w-8'></Image>
-            </a>
-            <a href="https://www.instagram.com/iecsemanipal/">
-                <Image src={linkedin}  className='h-8 w-8' ></Image>
-            </a>
-        </div>
+        <a href="https://www.instagram.com/iecsemanipal/">
+          <Image src={linkedin} className='h-8 w-8' alt='linkedin'></Image>
+        </a>
+      </div>
     </div>
   )
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,16 +5,16 @@ import CountdownTimer from "./CountdownTimer"
 
 const Timer = () => {
   return (<>
-    <div className="text-white w-full bg-[url('../assets/timer-back.svg')] relative pt-10 pb-9 mt-5 lg:flex justify-center">
-    {/* <Image src={right_circle} className="rotate absolute z-1 lg:hidden"></Image> */}
-    <div className="mt-40 mb-32 z-2">
-    <div className="text-4xl font-custom1 mb-5 text-center lg:text-5xl lg:mb-20">TIME IS RUNNING OUT</div>
-    <CountdownTimer
-        countdownTimestampMs={1712537430000} />
+    <div className="text-white w-full h-screen bg-[url('../assets/timer-back.svg')] relative pt-10 pb-9 mt-5 lg:flex justify-center">
+      {/* <Image src={right_circle} className="rotate absolute z-1 lg:hidden"></Image> */}
+      <div className="mt-40 mb-32 z-2">
+        <div className="text-4xl font-custom1 mb-5 text-center lg:text-5xl lg:mb-20">TIME IS RUNNING OUT</div>
+        <CountdownTimer
+          countdownTimestampMs={1712537430000} />
+      </div>
     </div>
-    </div>
-   
-    </>
+
+  </>
   )
 }
 
